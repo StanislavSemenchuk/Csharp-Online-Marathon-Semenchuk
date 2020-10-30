f7e1c18007aa09f3d1f03f20c3875cf69a2a3a09
StanislawSemenchuk <staniswlavsemenchuk@gmail.com>
2020-10-30 09:08:54 +0200

    Task-02 Completed

---
+++ Sprint-01/Task-02/Point.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task_02
+{
+    public class Point
+    {
+        private int x, y;
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        internal int[] GetXYPair()
+        {
+            return new int[] { x, y };
+        }
+
+        protected internal double Distance(int x, int y)
+        {
+            return Math.Sqrt(Math.Pow(this.x - x, 2) + Math.Pow(this.y - y, 2));
+        }
+
+        protected internal double Distance(Point point)
+        {
+            return Math.Sqrt(Math.Pow(this.x - point.x, 2) + Math.Pow(this.y - point.y, 2));
+        }
+
+        public static explicit operator double (Point point)
+        {
+            return Math.Sqrt(Math.Pow(point.x, 2) + Math.Pow(point.y, 2));
+        }
+    }
+
+}
