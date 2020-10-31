67ccaed11a5d8e19301734e057a4e1fb8af1584a
StanislawSemenchuk <staniswlavsemenchuk@gmail.com>
2020-10-31 10:10:36 +0200

    Task-04 completed

--- Sprint-01/Task-04/DisposePatternImplementer.cs
+++ Sprint-01/Task-04/DisposePatternImplementer.cs
@@ -2,8 +2,40 @@
 
 namespace Task_04
 {
-    public class DisposePatternImplementer : CloseableResource
+    public class DisposePatternImplementer : CloseableResource, IDisposable
     {
+        private bool disposedValue;
 
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                    Console.WriteLine("Disposing by developer");
+                    Close();
+                }
+                else
+                {
+                    Console.WriteLine("Disposing by GC");
+                    Close();
+                }
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        ~DisposePatternImplementer()
+        {
+            Dispose(disposing: false);
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
     }
 }
