500c0ec1f4c9a82b9127e2f5a3e98172dde9d43d
StanislawSemenchuk <staniswlavsemenchuk@gmail.com>
2020-11-02 18:25:05 +0200

    Sprint-02 Task-05 completed

---
+++ Sprint-02/Task-05/Acinonychini.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Task_05
+{
+    public class Acinonychini
+    {
+    }
+}
---
+++ Sprint-02/Task-05/Puma.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_05
+{
+    class Puma : Acinonychini
+    {
+    }
+}
