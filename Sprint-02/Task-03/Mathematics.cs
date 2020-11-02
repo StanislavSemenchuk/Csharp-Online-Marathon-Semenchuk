441c201b7401d0950f16cdd1e9ace004ff8373bf
StanislawSemenchuk <staniswlavsemenchuk@gmail.com>
2020-11-02 17:11:29 +0200

    Sprint-02 Task-03 completed

---
+++ Sprint-02/Task-03/Mathematics.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_03
+{
+    class Mathematics : Science
+    {
+        public override void Awards()
+        {
+            Console.WriteLine("We don't need any awards, but we still can obtain The Abel Prize that nobody else can");
+        }
+    }
+}
---
+++ Sprint-02/Task-03/Science.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task_03
+{
+    public class Science
+    {
+        public virtual void Awards() 
+        {
+            Console.WriteLine("We can obtain The Nobel Price");
+        }
+    }
+}
