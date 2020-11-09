4a0487de4c7f561930cb0a08301aa9900737cf28
StanislawSemenchuk <staniswlavsemenchuk@gmail.com>
2020-11-09 12:20:23 +0200

    Sprint-04 Task-2 completed

---
+++ Sprint-04/Task-02/IAnimal.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Task_01
+{
+    public interface IAnimal
+    {
+        public string Name { get; set; }
+        public void Voice();
+
+        public void Feed();
+    }
+
+    public class Dog : IAnimal
+    {
+        public string Name { get ; set ; }
+
+        public void Feed()
+        {
+            Console.WriteLine("I eat meat");
+        }
+
+        public void Voice()
+        {
+            Console.WriteLine("Woof");
+        }
+    }
+
+    public class Cat : IAnimal
+    {
+        public string Name { get; set; }
+
+        public void Feed()
+        {
+            Console.WriteLine("I eat mice");
+        }
+
+        public void Voice()
+        {
+            Console.WriteLine("Mew");
+        }
+    }
+}
