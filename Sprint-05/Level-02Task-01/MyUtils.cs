9a5be9fc23e67dd4a2faa98a4c8334989e3edf11
StanislawSemenchuk <staniswlavsemenchuk@gmail.com>
2020-11-13 19:05:10 +0200

    Sprint-05 Level-02 Task-01 completed

---
+++ Sprint-05/Level-02Task-01/MyUtils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Level_02Task_01
+{
+    public class MyUtils
+    {
+        public static bool ListDictionaryCompare(List<string> list, Dictionary<string, string> dictionary)  
+        {
+            var ListDict = dictionary.Select(dict => dict.Value).Distinct().ToList();
+            ListDict.Sort();
+            list = list.Distinct().ToList();
+            list.Sort();
+            return ListDict.SequenceEqual(list);
+        }
+    }
+}
