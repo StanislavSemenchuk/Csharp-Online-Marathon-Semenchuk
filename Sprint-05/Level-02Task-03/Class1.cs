3666ddd0e4943e4a197701e708df305b5489daa8
StanislawSemenchuk <staniswlavsemenchuk@gmail.com>
2020-11-13 20:20:13 +0200

    Sprint-05 Level-02 Task-03 completed

---
+++ Sprint-05/Level-02Task-03/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Level_02Task_03
+{
+    public class Class1
+    {
+        public static Dictionary<string, List<string>> ReverseNotebook(Dictionary<string, string> phonesToNames)
+        { 
+            var mylookup = (Lookup<string, string>)phonesToNames.ToLookup(ph => ph.Value ?? "", ph => ph.Key ?? "");
+            var mydict = mylookup.ToDictionary(x => x.Key, x => x.ToList());
+            return mydict;
+        }
+    }
+}
