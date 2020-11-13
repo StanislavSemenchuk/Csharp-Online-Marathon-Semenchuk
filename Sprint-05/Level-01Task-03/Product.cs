935fdbaec5f7866625e0ced31b0a58fd7c25e38e
StanislawSemenchuk <staniswlavsemenchuk@gmail.com>
2020-11-13 15:00:06 +0200

    Sprint-05 Level-01 Task-03 completed

---
+++ Sprint-05/Level-01Task-03/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Level_01Task_03
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+
+        public static void TotalPrice(ILookup<string, Product> lookup) 
+        {
+            foreach (var l in lookup) 
+            {
+                foreach (Product product in l)
+                {
+                    Console.WriteLine(product.Name + " " + product.Price);
+                }
+
+                Console.WriteLine(l.Key + " " + l.Sum(p => p.Price));
+            }
+        }
+    }
+}
