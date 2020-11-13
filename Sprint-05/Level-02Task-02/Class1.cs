655350807c634aed7c60e2848d9ebc4bbd3e5b12
StanislawSemenchuk <staniswlavsemenchuk@gmail.com>
2020-11-13 19:50:35 +0200

    Sprint-05 Level-02 Task-02 completed

---
+++ Sprint-05/Level-02Task-02/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Level_02Task_02
+{
+    public class Class1
+    {
+        public static Lookup<string, string> CreateNotebook(Dictionary<string, string> phonesToNames)
+        {
+            return (Lookup<string, string>)phonesToNames.ToLookup(ph => ph.Value ?? "", ph => ph.Key ?? "");
+        }
+    }
+}
