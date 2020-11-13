7835cc12309a6061b636007f8773f1cc3aad5f9c
StanislawSemenchuk <staniswlavsemenchuk@gmail.com>
2020-11-13 14:05:06 +0200

    Sprint-05 Level-01 Task-02 completed

---
+++ Sprint-05/Level-01Task-02/MyProgram.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Level_01Task_02
+{
+    public class MyProgram
+    {
+        public static void SearchKeys(Dictionary<string, string> persons) 
+        {
+            foreach (var person in persons) 
+            {
+                Console.WriteLine(person.Key);
+            }
+        }
+        public static void SearchValues(Dictionary<string, string> persons) 
+        {
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person.Value);
+            }
+        }
+        public static void SearchAdmin(Dictionary<string, string> persons) 
+        {
+            var AdminDictionary = persons.Where(p => p.Value == "Admin");
+            foreach (var Admin in AdminDictionary)
+            {
+                Console.WriteLine(Admin.Key + " " + Admin.Value);
+            }
+        }
+    }
+}
