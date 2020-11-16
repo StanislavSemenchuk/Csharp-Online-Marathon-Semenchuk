bccd5971c94a5dac48149ee1c63e6e8754eeced1
StanislawSemenchuk <staniswlavsemenchuk@gmail.com>
2020-11-16 19:30:49 +0200

    Sprint-06 Task-03 completed

---
+++ Sprint-06/Task-3/ShowPower.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Task_3
+{
+	public class ShowPower
+	{
+		public static IEnumerable<double> SuperPower(int numb, int deg) 
+		{
+			double result = 1;
+
+			if (numb == 0) 
+			{
+				yield return 0;
+			}
+			else if (deg > 0)
+			{
+				for (int i = 0; i < deg; i++)
+				{
+					result *= numb;
+					yield return result;
+				}
+			}
+			else if (deg == 0)
+			{
+				yield return result;
+			}
+			else 
+			{
+				for (int i = 0; i > deg; i--)
+				{
+					yield return result /= numb;
+				}
+			}
+		}
+	}
+}
+
