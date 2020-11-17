48a89378d714a42ea65e238e2edf89eb9cb9e7b2
StanislawSemenchuk <staniswlavsemenchuk@gmail.com>
2020-11-17 13:56:12 +0200

    Sprint-06 Task-04 completed

---
+++ Sprint-06/Task-04/ShowPowerRange.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_04
+{
+    public class ShowPowerRange
+    {
+        public static IEnumerable<double> PowerRanger(int deg, int start, int finish)
+        {
+            if (start > 0 && finish > 0 && start < finish && deg != 0)
+            {
+                int startRange = (int)Math.Ceiling(Math.Pow(start, 1.0 / deg));
+
+                while (Math.Pow(startRange, deg) <= finish)
+                {
+                    yield return Math.Pow(startRange, deg);
+                    startRange++;
+                }
+            }
+            else if (deg == 0)
+            {
+                yield return 1;
+            }
+            else
+            {
+                yield return 0;
+            }
+        }
+    }
+}
