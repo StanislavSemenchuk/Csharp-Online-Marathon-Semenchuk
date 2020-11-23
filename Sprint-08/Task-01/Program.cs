72bde2d8468274767fc40c9bdff4f31cf4a7699d
StanislawSemenchuk <staniswlavsemenchuk@gmail.com>
2020-11-23 17:37:02 +0200

    Sprint-07 Task-01 completed

---
+++ Sprint-08/Task-01/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Linq;
+
+namespace Task_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class ParallelUtils<T, TR>
+    {
+        Func<T, T, TR> func;
+        T operand1;
+        T operand2;
+        public ParallelUtils(Func<T,T,TR> func, T operand1, T operand2)
+        {
+            this.func = func;
+            this.operand1 = operand1;
+            this.operand2 = operand2;
+        }
+
+        public TR Result { get; set; }
+
+        public void StartEvaluation() 
+        {
+            Thread thread = new Thread(() =>
+            {
+                Result = func(operand1, operand2);
+            });
+            thread.Start();
+        }
+        public async void Evaluate() 
+        {
+            Result = func(operand1, operand2);
+        }
+    }
+}
