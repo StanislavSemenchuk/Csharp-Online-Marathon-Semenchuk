62706a5f1aae752372afda5422588685ff479218
StanislawSemenchuk <staniswlavsemenchuk@gmail.com>
2020-11-24 22:03:55 +0200

    Sprint-08 Task-01.02 completed

---
+++ Sprint-08/Task-02/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Task_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class MainThreadProgram
+    {
+        public static void Sum()
+        {
+            int[] num = new int[5];
+
+            for (int i = 1; i <= num.Length; i++)
+            {
+                switch (i)
+                {
+                    case 1:
+                        Console.WriteLine("Enter the 1st number:");
+                        num[i - 1] = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter the 2nd number:");
+                        num[i - 1] = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    default:
+                        Console.WriteLine($"Enter the {i}th number:");
+                        num[i - 1] = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            Console.WriteLine("Sum is: {0}", num.Sum());
+        }
+
+        public static void Product() 
+        {
+            List<int> list = new List<int>();
+            for (int i = 1; i <= 10; i++)
+            {
+                list.Add(i);
+            }
+            Thread.Sleep(10000);
+            Console.WriteLine("Product is: {0}", list.Aggregate((x, y) => x * y));
+        }
+
+        public static (Thread, Thread) Calculator() 
+        {
+            Thread sumThread = new Thread(Sum);
+            Thread productThread = new Thread(Product);
+            sumThread.Start();
+            productThread.Start();
+            return (sumThread, productThread);
+        }
+    }
+}
+
---
+++ Sprint-08/Task-03/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Task_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tasks();
+        }
+
+        public static void Tasks()
+        {
+            int[] sequense_array = new int[10];
+
+            Task[] tasks1 = new Task[3] {
+                new Task(()=>{
+                    for (int i = 0; i < 10; i++)
+                    {
+                        sequense_array[i] = i*i;
+                    }
+                    Console.WriteLine("Calculated!");
+                }),
+                new Task(()=>{
+                    for (int i = 0; i < 10; i++)
+                    {
+                        Console.WriteLine(i);
+                    }
+                    Console.WriteLine("Bye!");
+                }),
+                new Task(()=>{
+                    for (int i = 0; i < 10; i++)
+                    {
+                        Console.WriteLine(sequense_array[i]);
+                    }
+                    Console.WriteLine("Bye!");
+                })
+            };
+
+            for (int i = 0; i < tasks1.Length; i++)
+            {
+                tasks1[i].Start();
+                tasks1[i].Wait();
+            }
+
+            Task.WaitAll(tasks1);
+
+            Console.WriteLine("Main done!");
+        }
+    }
+}
