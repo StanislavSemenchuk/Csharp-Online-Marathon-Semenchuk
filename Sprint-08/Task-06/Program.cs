359fff4747be43696915cc60809eca8ab094966b
StanislawSemenchuk <staniswlavsemenchuk@gmail.com>
2020-11-25 21:43:22 +0200

    Sprint-08 Task-05 completed

---
+++ Sprint-08/Task-06/Program.cs
@@ -0,0 +1,47 @@
+﻿
+using System;
+using System.Threading;
+
+namespace Task_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class PersonInTheShop
+    {
+        public void Enter() { }
+
+        public void Pay() { }
+
+        public void Leave() { }
+
+        public void SelectGroceries() { }
+    }
+
+    class Buyer : PersonInTheShop
+    {
+        static Semaphore sem = new Semaphore(0, 10);
+
+        public Buyer(string treadName)
+        {
+            Thread buyerThread = new Thread(() =>
+            {
+                sem.WaitOne();
+
+                Enter();
+                SelectGroceries();
+                Pay();
+                Leave();
+
+                sem.Release();
+            })
+            { Name = treadName };
+            buyerThread.Start();
+        }
+    }
+}
