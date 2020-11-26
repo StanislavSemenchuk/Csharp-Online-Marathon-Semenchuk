10a6bffbe7fadb9f0289e4bdef674be20b0087da
StanislawSemenchuk <staniswlavsemenchuk@gmail.com>
2020-11-26 11:44:16 +0200

    Sprint-09 Task-01 completed

---
+++ Sprint-09/Task-01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Task_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class CalcAsync : Calc
+    {
+        public static async void PrintSeqAsync(int n) 
+        {
+            Console.WriteLine("Seq[{0}] = {1}", n, await Task.Run(() => Seq(n)));
+        }
+    }
+
+    class Calc
+    {
+        public static int Seq(int n) { return 0; }
+    }
+}
